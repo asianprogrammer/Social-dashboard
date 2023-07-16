debbfd77e0a11b1c6ea412d7a028bcf41cd23ccd
It's Me <itsme@sorry.com>
2023-07-16 19:46:05 +0600

    ✨ Icon for this project

---
+++ src/components/icons/NaveIcon.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg width="40" height="40" viewBox="0 0 24 24">
+    <circle cx="12" cy="12" r="2" />
+    <circle cx="12" cy="5" r="2" />
+    <circle cx="12" cy="19" r="2" />
+  </svg>
+</template>
---
+++ src/components/icons/UpRightIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    width="40"
+    height="40"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="#000000"
+    stroke-width="1"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M7 17l9.2-9.2M17 17V7H7" />
+  </svg>
+</template>
